fcec0cec86f58f5724f1cb4eb00a3d125509e8d6
volidenko <55356026+volidenko@users.noreply.github.com>
2021-02-22 00:09:09 +0200

    comm

---
+++ ChangeColor.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeColor : MonoBehaviour
+{
+    public Material[] mat_s;
+    public void OnMouseDown() {
+        gameObject.GetComponent<Render>().material=mat_s[Random.Range(0, mat_s.Length)]);
+    }
+}
---
+++ PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    public float charSpeed=1f;
+    public float turnSpeed=50f;
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if(Input.GetKey(KeyCode.W))
+            transform.Translate(Vector3.forward*charSpeed*Time.deltaTime);
+        if(Input.GetKey(KeyCode.S)
+            transform.Translate(Vector3.back*charSpeed*Time.deltaTime);
+        if(Input.GetKey(KeyCode.A)
+            transform.Translate(Vector3.down*turnSpeed*Time.deltaTime);
+        if(Input.GetKey(KeyCode.S)
+            transform.Translate(Vector3.up*turnSpeed*Time.deltaTime);
+    }
+}
