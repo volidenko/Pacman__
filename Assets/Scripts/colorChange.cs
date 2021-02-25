4b1407c23a77ef21bc0043bce4c4a00ed00f46ba
volidenko <volidenko@.i.ua>
2021-02-25 19:07:02 +0200

    comm

--- Assets/Scripts/colorChange.cs
+++ Assets/Scripts/colorChange.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 
 public class colorChange : MonoBehaviour
 {
+    public Material[] mat_s;
+    public void OnMouseDown() {
+        gameObject.GetComponent<Renderer>().material=mat_s[Random.Range(0, mat_s.Length)];
+    }
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
